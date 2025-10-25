fa957de8176c0de47467211053a88212250156a3
Dapinder-Kaur <gilldapinder515@gmail.com>
2025-10-25 17:16:10 -0500

    update status variable

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 import os
 from dotenv import load_dotenv
-
+import requests
 
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
 
+check_service = f"https://api.winnipegtransit.com/v2/statuses/schedule.json?api-key={api_key}"
+print(check_service)
+
+response  = requests.get(check_service)
+
+res_dir = (response.json())
+
+res_dir_keys = res_dir.keys()
 # print(f"API Key: {api_key}")
+
+res_dir_status = res_dir['status']
+
+print(res_dir_status['message'])
